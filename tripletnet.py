78c1b46d53e6ce07e11d2b4d90daf6dd4f370893
peternara <peternara@naver.com>
2020-04-18 12:24:07 +0000

    csn anlaysis 2

--- tripletnet.py
+++ tripletnet.py
@@ -12,12 +12,16 @@ class CS_Tripletnet(nn.Module):
             y: Distant (negative) image,
             z: Close (positive) image,
             c: Integer indicating according to which notion of similarity images are compared"""
+
         embedded_x, masknorm_norm_x, embed_norm_x, tot_embed_norm_x = self.embeddingnet(x, c)
         embedded_y, masknorm_norm_y, embed_norm_y, tot_embed_norm_y = self.embeddingnet(y, c)
         embedded_z, masknorm_norm_z, embed_norm_z, tot_embed_norm_z = self.embeddingnet(z, c)
-        mask_norm = (masknorm_norm_x + masknorm_norm_y + masknorm_norm_z) / 3
-        embed_norm = (embed_norm_x + embed_norm_y + embed_norm_z) / 3
+
+        mask_norm       = (masknorm_norm_x + masknorm_norm_y + masknorm_norm_z) / 3
+        embed_norm      = (embed_norm_x + embed_norm_y + embed_norm_z) / 3
         mask_embed_norm = (tot_embed_norm_x + tot_embed_norm_y + tot_embed_norm_z) / 3
+
         dist_a = F.pairwise_distance(embedded_x, embedded_y, 2)
         dist_b = F.pairwise_distance(embedded_x, embedded_z, 2)
+
         return dist_a, dist_b, mask_norm, embed_norm, mask_embed_norm
